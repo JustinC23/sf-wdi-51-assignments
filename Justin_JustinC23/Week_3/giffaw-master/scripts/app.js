cf3d317e4e104deddc2cbf0447739004d0d6a60a
Justinc23 <cullen.justin23@gmail.com>
2019-02-07 14:47:52 -0800

    pushing project 00

--- Justin_JustinC23/Week_3/giffaw-master/scripts/app.js
+++ Justin_JustinC23/Week_3/giffaw-master/scripts/app.js
@@ -1,27 +1,33 @@
-    var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + userInput + '&api_key=5Gn9M4KG9WUWt8UzRFMo5Lo39L1Bi9vp';
+var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + '&api_key=5Gn9M4KG9WUWt8UzRFMo5Lo39L1Bi9vp';
 
   $(document).ready(function(){
-    var key = '&api_key=5Gn9M4KG9WUWt8UzRFMo5Lo39L1Bi9vp'
+    var key = '&api_key=5Gn9M4KG9WUWt8UzRFMo5Lo39L1Bi9vp';
   // code in here!
-  console.log("here's your data:",)
-})
+  console.log("here's your data:");
+});
 
   $('form-inline').on('submit', function(e) {
   e.preventDefault();
 
-let gifs = gifs.data[0].images
+let gifs = gifs.data[0].images;
 $('.search_button').on('click', function(){
+});
 
 let userInput =  $('.form-control gif-input').val();
     //grab user value from input field
 
+
   $.ajax({
     method: "GET",
-    url: 'queryURL',
+    url: 'queryURL' + 'userInput',
     success: gifSuccess,
     error: gifError
-   })
+   });
 });
+
+
+
+
 
 function gifSuccess(gifs){
   console.log(gifs);
@@ -29,7 +35,7 @@ function gifSuccess(gifs){
       $('.gif-gallery').append(`<img src=${gifs}<img/>`);
   });
   $('.gif-input').val("");
-};
+}
 
 function handleError(e1, e2, e3){
   console.log(e1);
