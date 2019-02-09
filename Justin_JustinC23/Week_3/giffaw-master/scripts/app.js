a5d1599941c20eadadf47bf3a1ca00eef75fba29
Justinc23 <cullen.justin23@gmail.com>
2019-02-09 15:38:00 -0800

    added form

--- Justin_JustinC23/Week_3/giffaw-master/scripts/app.js
+++ Justin_JustinC23/Week_3/giffaw-master/scripts/app.js
@@ -1,45 +1,41 @@
-var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + '&api_key=5Gn9M4KG9WUWt8UzRFMo5Lo39L1Bi9vp';
+var queryURL = 'http://api.giphy.com/v1/gifs/search?q=&api_key=5Gn9M4KG9WUWt8UzRFMo5Lo39L1Bi9vp';
+    var key = '&api_key=5Gn9M4KG9WUWt8UzRFMo5Lo39L1Bi9vp';
 
   $(document).ready(function(){
-    var key = '&api_key=5Gn9M4KG9WUWt8UzRFMo5Lo39L1Bi9vp';
   // code in here!
   console.log("here's your data:");
 });
 
   $('form-inline').on('submit', function(e) {
+    //prevent default action of button
   e.preventDefault();
 
-let gifs = gifs.data[0].images;
-$('.search_button').on('click', function(){
-});
-
-let userInput =  $('.form-control gif-input').val();
-    //grab user value from input field
-
+let userInput = $('.gif-input').val();
 
   $.ajax({
     method: "GET",
     url: 'queryURL' + 'userInput',
     success: gifSuccess,
-    error: gifError
-   });
+    error: handleError
+
+
+let gifList = response.data;
+let gifs = gifdata.images.original.url;
+$('.search_button').on('click', function(){
 });
 
 
+for (gifs of ` )
 
+    gifHTML = `<div class = "gif-gallery">
+    <img src="${gif.images.original.url}" alt="${ gif.title }">
+    </div>
+ }
 
-
-function gifSuccess(gifs){
-  console.log(gifs);
-    gifs.images.forEach(function(gif){
-      $('.gif-gallery').append(`<img src=${gifs}<img/>`);
-  });
-  $('.gif-input').val("");
-}
-
-function handleError(e1, e2, e3){
+function handleError(e1){
   console.log(e1);
-  console.log(e2);
-  console.dir(e3);
 }
+
+   });
+
 
