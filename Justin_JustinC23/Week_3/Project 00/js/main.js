4d6c1060f0e4dbcabb2adb79a041400fb73875ad
Justinc23 <cullen.justin23@gmail.com>
2019-02-11 23:52:35 -0800

    adding project 00 to submit

--- Justin_JustinC23/Week_3/Project 00/js/main.js
+++ Justin_JustinC23/Week_3/Project 00/js/main.js
@@ -1,12 +1,28 @@
 $(function(){
-  var items = (Math.floor(Math.random() * ($('#testimonials li').length)));
-  $('#testimonials li').hide().eq(items).show();
+  var items = (Math.floor(Math.random() * ($('#testimonials li').length))); //multiply our randomly generated number by another number, it will range from 0 to a maximum of 1 lower than the number being multiplied by (as Math.floor() simply removes the decimal places rather than rounding the number (that is to say, 0.999 becomes 0 when processed with Math.floor(), not 1)).
+  $('#testimonials li').hide().eq(items).show();   //shows list items constructs new item with items
+
 
   function next(){
-    $('#testimonials li:visible').delay(5000).fadeOut('slow',function(){
+    $('#testimonials li:visible').delay(4000).fadeOut('slow',function(){
       $(this).appendTo('#testimonials ul');
       $('#testimonials li:first').fadeIn('slow',next);
     });
    }
   next();
 });
+
+$("#lightbox_trigger").click(function(){
+  $("#lightbox").show();
+});
+
+$(function() {
+
+    $(".nav-item").on('click', function() {
+        $("h3").css('text-decoration', 'underline');
+        $(this).css('text-decoration', 'underline');
+    });
+
+});
+
+
---
+++ Justin_JustinC23/Week_3/Project 00/readme1.md
@@ -0,0 +1,16 @@
+Tech Used
+  Bootstrap jquery plug in scrollnav for scroll feature
+
+  Most challenging, surprisingly getting lightbox to work obviously
+  i started to code it and would finish that next.
+
+  Process was to figure out how Bootstrap works as it was my first endeavor in
+  bootstrap. I have been looking forward to using it. To further utilize Bootstrap,
+  I used it to stylize my nav and for columns throughout the html. I then made my forms.
+  I like seeing the structure of my page early even if its minimal elements.
+I then went into thinking about my testimonials and how I would do them.
+
+
+  Unfinished, styling my css to make page look more professional. Adding more
+  intuitive functionality to form and different sections. Adding a skill bar
+  using css/jquery animations which is what I wanted to originally do before using fa icons.
